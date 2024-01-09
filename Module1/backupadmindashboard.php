9d8d7caa7e4f789d45f937947486ea4d77897e11
tang jimmy <merckainx3@gmail.com>
2024-01-09 19:19:09 +0800

    m

--- Module1/backupadmindashboard.php
+++ Module1/backupadmindashboard.php
@@ -1,11 +1,11 @@
+
+
 <?php
     session_start();
     include("config.php");
     
     
 ?>
-
-
 
 <!DOCTYPE html>
 <html lang="en">
@@ -18,30 +18,28 @@
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0" />
     <title>Pie Chart in Php</title>
     <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
-
     <script type="text/javascript">
-            <?php
-            echo "var counts = [";
-            echo "['User Type', 'Count'],";
-            
-            $queryAdmin = "SELECT COUNT(*) as count FROM administrator";
-            $resultAdmin = mysqli_query($con, $queryAdmin);
-            $countAdmin = mysqli_fetch_assoc($resultAdmin)['count'];
-            echo "['Administrator', $countAdmin],";
-            
-            $queryVendor = "SELECT COUNT(*) as count FROM food_vendor";
-            $resultVendor = mysqli_query($con, $queryVendor);
-            $countVendor = mysqli_fetch_assoc($resultVendor)['count'];
-            echo "['Food Vendor', $countVendor],";
-            
-            $queryRegisteredUser = "SELECT COUNT(*) as count FROM registered_user";
-            $resultRegisteredUser = mysqli_query($con, $queryRegisteredUser);
-            $countRegisteredUser = mysqli_fetch_assoc($resultRegisteredUser)['count'];
-            echo "['Registered User', $countRegisteredUser]";
-            
-            echo "];";
-            ?>
+   <?php
+   echo "var counts = [";
+   echo "['User Type', 'Count'],";
+   
+   $queryAdmin = "SELECT COUNT(*) as count FROM administrator";
+   $resultAdmin = mysqli_query($con, $queryAdmin);
+   $countAdmin = mysqli_fetch_assoc($resultAdmin)['count'];
+   echo "['Administrator', $countAdmin],";
+   
+   $queryVendor = "SELECT COUNT(*) as count FROM food_vendor";
+   $resultVendor = mysqli_query($con, $queryVendor);
+   $countVendor = mysqli_fetch_assoc($resultVendor)['count'];
+   echo "['Food Vendor', $countVendor],";
+   
+   $queryRegisteredUser = "SELECT COUNT(*) as count FROM registered_user";
+   $resultRegisteredUser = mysqli_query($con, $queryRegisteredUser);
+   $countRegisteredUser = mysqli_fetch_assoc($resultRegisteredUser)['count'];
+   echo "['Registered User', $countRegisteredUser]";
+   
+   echo "];";
+   ?>
 
    google.charts.load('current', {'packages':['corechart']});
    google.charts.setOnLoadCallback(drawChart);
@@ -50,7 +48,14 @@
       var data = google.visualization.arrayToDataTable(counts);
 
       var options = {
-         title: 'User Types Distribution'
+         title: 'User Types Distribution',
+                backgroundColor: {
+                fill: '#fbe3e8', // Set your desired background color
+                stroke: '#d9d9d9', // Set the border color if needed
+                strokeWidth: 0 // Set the border width if needed
+    }
+         
+         
       };
 
       var chart = new google.visualization.PieChart(document.getElementById('piechart'));
@@ -58,21 +63,23 @@
       chart.draw(data, options);
    }
 </script>
+
+
+
 </head>
 <body>
     <div class="hero">
         <nav>
-            <a href="home.php" class="logo"></a>
+            <a href="homeadmin.php" class="logo"></a>
             
           
                     
 
             
             <ul>
-                <li><a href="homeadmin.php">Home</a></li>
                 <li><a href="admindashboard.php">Dashboard</a></li>
-                <li><a href="#">About</a></li>
-
+                <li><a href="Userlist.php">User list</a></li>
+                <li><a href="pending.php">Application pending list</a></li>
                 <li><a href="#">Contact</a></li>
             </ul>
             <img src="login.png" class="user-pic" onclick="toggleMenu()">
@@ -83,7 +90,34 @@
                 
                     <div class="user-info">
                         <img src="login.png" style="margin-right: 10px;">
-                        <h3>Hai Lat</h3> 
+                        <?php
+                        $id=$_SESSION['id'];
+                        
+                        
+                        $res_name=null;
+                        $query= mysqli_query($con, "SELECT * FROM administrator where ID='$id'");
+                        if (!$query) {
+                            die("Query failed: " . mysqli_error($con));
+                        }
+                    
+                        // Check if there are any results
+                        if ($result = mysqli_fetch_assoc($query)) {
+                            $res_name = $result['Name'];
+                            echo "<h3>$res_name</h3>";
+                        } else {
+                            echo "<h3>No Name Found</h3>"; // or handle the case where the name is not found
+                        }
+                        
+                    
+    ?>
+                        
+                       
+                        
+                        <!-- $id=$_SESSION['valid'];
+                        $nama= mysqli_query($con, "SELECT Name FROM administrator where ID='$id'");
+                        echo "<h3>$nama</h3>";
+                        -->
+
 
 
                     </div>
@@ -91,7 +125,7 @@
                     <div>
                     <?php
 
-                    $id=$_SESSION['valid'];
+                    $id=$_SESSION['id'];
                     $query= mysqli_query($con, "SELECT * FROM administrator where ID='$id'");
 
                     $res_id=null;
@@ -108,22 +142,7 @@
 
                     }
 
-                    echo "<a href='login.php' class='sub-menu-link'>
-                        <img src='./icon/login.png'>
-                        <p>Login</p>
-                        <span>></span>
-
-                    </a>";
-
                     
-
-
-                    echo "<a href='register.php' class='sub-menu-link'>
-                        <img src='./icon/signin.png' style='height: 40px; width: 40px;'>
-                        <p>Sign In</p>
-                        <span>></span>
-
-                    </a>";
 
                    
                      echo "<a href='editadmin.php?Id=$res_id' class='sub-menu-link'>
@@ -131,7 +150,6 @@
                     <p>Edit Profile</p>
                     <span>></span>
                     </a>";
-                    
 
 
 
@@ -158,39 +176,9 @@
 
         </nav>
     </div>
-    </body>
-    <body class="bg-dark">
-            <div class="container-fluid">
-                <div class="row">
-                    <div class="col-lg-6 m-auto">
-                        <div class="card mt-5">
-
-                                <div class="card-header">
-                                    <h3>User Types Distribution</h3>
-
-                                </div>
-                                <div class="card-body">
-                                    <div id="piechart" style="width: 400px; height: 200px;"></div>
-
-                                </div>
-                                <div class="card-footer">
 
 
-                                </div>
-
-                        </div>
-
-
-                    </div>
-
-
-                </div>
-
-            
-            </div>
-
-            
-    </body>
+    <div id="piechart" style="width: 900px; height: 500px;"></div>
 <script>
     let subMenu=document.getElementById("subMenu");
 
@@ -202,8 +190,7 @@
 
 </script>
 
-
+</body>
 
 
 </html>
-
